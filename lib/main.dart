b286f77dd376097eefb48d27554c0a18aca7f3a6
rubenshibu <rubenshibu@gmail.com>
2020-12-10 18:23:32 +0530

    bug fixed:bug:

--- lib/main.dart
+++ lib/main.dart
@@ -6,17 +6,16 @@ void main() {
   runApp(MyApp());
 }
 
-
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'WhatsApp Link Generator',
+      // title: 'WhatsApp Link Generator',
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.teal,
       ),
-      home: MyHomePage(title: 'Link Generator'),
+      home: MyHomePage(title: 'Whatsapp Link Generator'),
       debugShowCheckedModeBanner: false,
     );
   }
@@ -25,6 +24,7 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
   final String title;
+
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
@@ -37,7 +37,6 @@ class _MyHomePageState extends State<MyHomePage> {
       _counter++;
     });
   }
-
   TextEditingController _phone = new TextEditingController();
   TextEditingController _message = new TextEditingController();
 
@@ -55,7 +54,6 @@ class _MyHomePageState extends State<MyHomePage> {
               child: new Text("Share"),
               onPressed: () {
                 Share.share("My WhatsApp: ${url}");
-
                 Navigator.of(context).pop();
               },
             ),
@@ -73,74 +71,81 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Padding(
         padding: EdgeInsets.all(12),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-
-              TextField(
-                decoration: new InputDecoration(
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 100.0,),
+            TextField(
+              decoration: new InputDecoration(
                   border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12.0)
-                  ),
-                    labelText: 'WhatsApp phone number',
-                    hintText: 'Number',
-                    icon: new Icon(Icons.phone, size: 24,)
-                ),
-                keyboardType: TextInputType.phone,
-                controller: _phone,
-              ),
-              SizedBox(height: 12,),
-
-              TextField(
-                decoration: new InputDecoration(
+                      borderRadius: BorderRadius.circular(12.0)),
+                  labelText: 'WhatsApp phone number',
+                  hintText: 'Number',
+                  icon: new Icon(
+                    Icons.phone,
+                    size: 24,
+                  )),
+              keyboardType: TextInputType.phone,
+              controller: _phone,
+            ),
+            SizedBox(
+              height: 25.0,
+            ),
+            TextField(
+              decoration: new InputDecoration(
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(12.0),
                   ),
-                    labelText: 'Message',
-                    hintText: 'Message',
-                    icon: new Icon(Icons.message, size: 24,)
-                ),
-                autocorrect: true,
-                keyboardType: TextInputType.text,
-                controller: _message,
+                  labelText: 'Message',
+                  hintText: 'Message',
+                  icon: new Icon(
+                    Icons.message,
+                    size: 24,
+                  )),
+              autocorrect: true,
+              keyboardType: TextInputType.text,
+              controller: _message,
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            Text("Any Simple message"),
+            SizedBox(
+              height: 12,
+            ),
+            RaisedButton(
+              color: Colors.teal,
+              child: Text(
+                "send",
+                style: TextStyle(color: Colors.white),
               ),
-              SizedBox(height: 12,),
-              Text("Any Simple message"),
-              SizedBox(height: 12,),
-              RaisedButton(
-                color: Colors.green,
-                child: Text("Link", style: TextStyle(color: Colors.white),),
-                onPressed: (){
-
-                  // String url = "https://wa.me/${_phone.text}?text=${_message.text}";
-                  String url = "wa.me/+91${_phone.text}?text=${_message.text}";
-
-                  _launchURL(context, url);
-                  // _showDialog(url);
-                },
-              ),
-
-              Expanded(
-                child:  Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: <Widget>[
-                    Text('team T E R M I N A L ',
-                      style: TextStyle(
-                        fontSize: 10.0,
-                      ),
+              onPressed: () {
+                // String url = "https://wa.me/${_phone.text}?text=${_message.text}";
+                String url = "wa.me/+91${_phone.text}?text=${_message.text}";
+                _launchURL(context, url);
+                // _showDialog(url);
+              },
+            ),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  Text(
+                    'team T E R M I N A L ',
+                    style: TextStyle(
+                      fontSize: 10.0,
                     ),
-                    Icon(Icons.favorite,size: 15.0,
-                    ),
-                  ],
-                ),
+                  ),
+                  Icon(
+                    Icons.favorite,
+                    size: 15.0,
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
-
   }
 }
 
