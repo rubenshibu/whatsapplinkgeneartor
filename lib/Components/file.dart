2faf518522ad5bdbcb34afcc54fa3f5ab394bc1b
rubenshibu <rubenshibu@gmail.com>
2021-01-08 22:22:55 +0530

    new :sparkles:

--- lib/Components/file.dart
+++ lib/Components/file.dart
@@ -1,4 +1,8 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:file_picker/file_picker.dart';
 
 main() => runApp(File());
 
@@ -8,7 +12,25 @@ class File extends StatefulWidget {
 }
 
 class _FileState extends State<File> {
+  String _fileName;
+  String _path;
+  Map<String, String> _paths;
+  String _extention;
+  bool _loadingPath = false;
+  bool _multiPick = false;
+  bool _hasValidMime = false;
+  FileType _pickingType;
+  TextEditingController _controller = new TextEditingController();
   @override
+  void initState(){
+    _controller.addListener(() => _extention = _controller.text );
+  }
+  void _openFileExplorer() async {
+    if(_pickingType != FileType.custom || _hasValidMime){
+      
+    }
+  }
+
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
