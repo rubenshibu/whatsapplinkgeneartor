f52fcd225e0176e2463ce5a7f9615e8a28957d1f
rubenshibu <rubenshibu@gmail.com>
2021-01-08 23:15:01 +0530

    new :sparkles:

--- lib/Components/file.dart
+++ lib/Components/file.dart
@@ -1,69 +1,118 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:file_picker/file_picker.dart';
+import 'package:flutter/services.dart';
 
-main() => runApp(File());
+main() => runApp(App());
 
-class File extends StatefulWidget {
+class App extends StatefulWidget {
   @override
-  _FileState createState() => _FileState();
+  State<StatefulWidget> createState() {
+    return _App();
+  }
 }
 
-class _FileState extends State<File> {
+class _App extends State<App> {
   String _fileName;
   String _path;
   Map<String, String> _paths;
-  String _extention;
+  String _extension;
   bool _loadingPath = false;
   bool _multiPick = false;
   bool _hasValidMime = false;
   FileType _pickingType;
   TextEditingController _controller = new TextEditingController();
+
   @override
   void initState() {
-    _controller.addListener(() => _extention = _controller.text);
+    super.initState();
+    _controller.addListener(() => _extension = _controller.text);
   }
 
   void _openFileExplorer() async {
-    if (_pickingType != FileType.custom || _hasValidMime) {
+    if (_pickingType != FileType.CUSTOM || _hasValidMime) {
       setState(() => _loadingPath = true);
       try {
         if (_multiPick) {
           _path = null;
-          _paths = await FilePicker.getMultipleFilePath(
-              type: _pickingType, fileExtention: _extention);
+          _paths = await FilePicker.getMultiFilePath(
+              type: _pickingType, fileExtension: _extension);
         } else {
           _paths = null;
           _path = await FilePicker.getFilePath(
-              type: _pickingType, fileExtenetion: _extention);
+              type: _pickingType, fileExtension: _extension);
         }
       } on PlatformException catch (e) {
-        print("Unsupported" + e.toString());
+        print("Unsupported operation" + e.toString());
       }
       if (!mounted) return;
       setState(() {
         _loadingPath = false;
         _fileName = _path != null
             ? _path.split('/').last
-            : _paths != null
-                ? _paths.keys.toString()
-                : '...';
+            : _paths != null ? _paths.keys.toString() : '...';
       });
     }
   }
 
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Add Files"),
+          title: Text("FilePickerApp"),
         ),
         body: Container(
           child: Center(
-            child: Text("code"),
-          ),
+              child: Column(
+            children: <Widget>[
+              new Padding(
+                padding: const EdgeInsets.only(top: 50.0, bottom: 20.0),
+                child: new RaisedButton(
+                  onPressed: () => _openFileExplorer(),
+                  child: new Text("Open file picker"),
+                ),
+              ),
+              new Builder(
+                builder: (BuildContext context) => _loadingPath
+                    ? Padding(
+                        padding: const EdgeInsets.only(bottom: 10.0),
+                        child: const CircularProgressIndicator())
+                    : _path != null || _paths != null
+                        ? new Container(
+                            padding: const EdgeInsets.only(bottom: 30.0),
+                            height: MediaQuery.of(context).size.height * 0.50,
+                            child: new Scrollbar(
+                                child: new ListView.separated(
+                              itemCount: _paths != null && _paths.isNotEmpty
+                                  ? _paths.length
+                                  : 1,
+                              itemBuilder: (BuildContext context, int index) {
+                                final bool isMultiPath =
+                                    _paths != null && _paths.isNotEmpty;
+                                final String name = 'File $index: ' +
+                                    (isMultiPath
+                                        ? _paths.keys.toList()[index]
+                                        : _fileName ?? '...');
+                                final path = isMultiPath
+                                    ? _paths.values.toList()[index].toString()
+                                    : _path;
+
+                                return new ListTile(
+                                  title: new Text(
+                                    name,
+                                  ),
+                                  subtitle: new Text(path),
+                                );
+                              },
+                              separatorBuilder:
+                                  (BuildContext context, int index) =>
+                                      new Divider(),
+                            )),
+                          )
+                        : new Container(),
+              ),
+            ],
+          )),
         ),
       ),
     );
