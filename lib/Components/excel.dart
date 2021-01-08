ea071cd39f1fc72255f9974977b1d38705636179
rubenshibu <rubenshibu@gmail.com>
2021-01-08 19:39:00 +0530

    new :sparkles:

--- lib/Components/excel.dart
+++ lib/Components/excel.dart
@@ -1,1 +1,26 @@
-import 'dart:io';
+import 'package:flutter/material.dart';
+
+main() => runApp(App());
+
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Add Files"),
+        ),
+        body: Container(
+          child: Center(
+            child: Text("code"),
+          ),
+        ),
+      ),
+    );
+  }
+}
